c47f5ac119a14ae68657d5ef0ed90b9f0f782416
Yunwen Zheng <ynn.zheng@gmail.com>
2020-02-23 17:01:00 -0500

    created sign-up component and intergrated with firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,6 +17,31 @@ const config = {
     measurementId: "G-Y30HZ8LNXZ"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef;
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
