c2a010269fcdd7272fb813612bc06a501243833b
Yunwen Zheng <ynn.zheng@gmail.com>
2020-02-24 18:59:47 -0500

    added increase and decrease item quantity on checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,16 +1,35 @@
 // Utility funcions allow us to keep our files clean
 // and organize functions that we may need in multiple files in one location
 
+  
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
+    );
+  
     if (existingCartItem) {
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id
-            ? { ...cartItem, quantity: cartItem.quantity + 1}
-            : cartItem
-            )
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
     }
-
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
-}
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
+  
+  export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToRemove.id
+    );
+  
+    if (existingCartItem.quantity === 1) {
+      return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+  
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  };
