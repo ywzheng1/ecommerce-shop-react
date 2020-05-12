96bdf4968395470e0b348c399280211720416f41
Yunwen Zheng <ynn.zheng@gmail.com>
2020-05-11 22:49:27 -0400

    reformate code

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -15,7 +15,9 @@ const CartDropdown = ( {cartItems, history, dispatch} ) => (
         <div className='cart-items'>
             {
                 cartItems.length ? 
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+                cartItems.map(cartItem => 
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                )
                 :
                 <span className='empty-message'>Your cart is empty</span>
             }
